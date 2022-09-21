f17d14c50e5b05d840ae200b4f843d9577765ac6
RobsenGelana <robsengelana21@gmail.com>
2022-09-21 18:27:11 +0300

    creating a snake movement and removing line when the snake moves

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+#Creating a snake body with three square shape side by side
 starting_position = [(0,0), (-20, 0), (-40, 0)]
+snakes = []
 for position in starting_position:
-    turtle = Turtle('square')
-    turtle.color('white')
-    turtle.goto(position)
+    snake = Turtle('square')
+    snake.color('white')
+    snake.penup()
+    snake.goto(position)
+    snakes.append(snake)
 
+game_on = True
+
+while game_on:
+    for snake in snakes:
+        snake.forward(20)
+        
 
 
 
