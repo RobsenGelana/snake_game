0913e75cddace29897fc4b9326669bedf7b6f310
RobsenGelana <robsengelana21@gmail.com>
2022-09-21 18:38:02 +0300

    importing the time model and updating the screen every time the snake make a movement

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from turtle import Turtle, Screen
-
+import time
 
 screen = Screen()
 #creating a screen size for the game
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 #Creating a snake body with three square shape side by side
 starting_position = [(0,0), (-20, 0), (-40, 0)]
@@ -20,6 +21,8 @@ for position in starting_position:
 game_on = True
 
 while game_on:
+    screen.update()
+    time.sleep(0.1)
     for snake in snakes:
         snake.forward(20)
         
