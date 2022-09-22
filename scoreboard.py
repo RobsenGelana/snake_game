49dd99fd8e8367e70950e712530a8cc1788754b8
RobsenGelana <robsengelana21@gmail.com>
2022-09-22 21:31:38 +0300

    creating a game over function

--- scoreboard.py
+++ scoreboard.py
@@ -12,7 +12,9 @@ class ScoreBoard(Turtle):
 
     def update_screen(self):
         self.write(f"Score: {self.score}", align='center',  font=('Arial', 25, 'normal'))
-
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game Over", align='center',  font=('Arial', 25, 'normal'))
     def increase_score(self):
         self.score += 1
         self.clear()
